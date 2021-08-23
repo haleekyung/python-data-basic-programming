f25ccab68c33fec60c92d41a66001ea550b161f6
haleekyung <kyungha03@gmail.com>
2021-08-24 00:29:30 +0900

    add more codes and upload data

--- Doit-2-basic/4-2-csv-data.py
+++ Doit-2-basic/4-2-csv-data.py
@@ -25,3 +25,18 @@ def opencsv(filename):
 
 opencsv('a.csv')
 
+a = [['구', '전체', '내국인', '외국인'],
+['관악구', '519864', '502089', '17775'],
+['강남구', '547602', '542498', '5104'],
+['송파구', '686181', '679247', '6934'],
+['강동구', '428547', '424235', '4312']]
+
+f = open('abc.csv', 'w', newline = '')
+csvobject = csv.writer(f, delimiter = ',')
+csvobject.writerows(a)
+f.close()
+
+def writecsv(filename, the_list):
+     with open(filename, 'w', newline = '') as f:
+     a = csv.writer(f, delimiter = ',')
+     a.writerows(the_list)
