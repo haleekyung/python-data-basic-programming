2f206a02533526e0bf1b751bb1423e11eb1f9844
haleekyung <kyungha03@gmail.com>
2021-09-03 00:17:00 +0900

    modify file

--- Doit-2-basic/4-4-csv-foreign.py
+++ Doit-2-basic/4-4-csv-foreign.py
@@ -6,30 +6,59 @@ os.chdir(r'/Users/leekyungha/PycharmProjects/python-data-basic-programming/Data'
 # 파일 열기
 total = usecsv.opencsv('popSeoul.csv')
 
-# 콤마 등을 제거하는 함수 usecsv.switch를 사용합니다.
+# 콤마 등을 제거하는 함수 usecsv.switch를 사용
 newPop = usecsv.switch(total)
-# 4번째 구까지만 시험으로 출력해봅니다.
+
+# 4번째 구까지만 시험으로 출력
 print(newPop[:4])
 
-for i in newPop:
-	foreign = 0
-## 한 번 루프가 돌고 나면 foreign을 다시 지정해주어야 하므로 foreign을 0으로 먼저 지정합니다
-	try:
-		foreign = round(i[2] / (i[1] + i[2]) * 100, 1)
-		print(i[0], foreign)
-## i[0]에는 지역구 이름이 저장되어 있고, foreign은 공식대로 우리가 계산한 외국인 비율입니다
-	except:
-		pass
-new = [['구', '한국인', '외국인', '외국인 비율(%)']]
-# 등록외국인의 비율이 3이 넘을 때만 넘을 때만 출력합니다.
-for i in newPop:
-        foreign = 0
-        try:
-            foreign = round(i[2] / (i[1] + i[2]) * 100, 1)
-            if foreign > 3:
-                new.append([i[0], i[1],i[2],foreign])
-        except:
-            pass
+# 외국인 등록 비율 테스트
+i = newPop[1]
+print(i)
 
-# 3% 넘는 구만 파일로 저장하기
-usecsv.writecsv('newPop.csv',new)
+cal = i[2] / (i[1] + i[2]) * 100
+print(cal)
+
+## 소수점 첫째자리까지만 저장할 것
+cal = round(i[2] / (i[1] + i[2]) * 100, 1)
+print(cal)
+
+# 각 구의 외국인 비율 출력하기
+for i in newPop:
+    foreign = 0
+    try:
+        foreign = round(i[2] / (i[1] + i[2] * 100, 1))
+        print(i[0], foreign)
+
+    except:
+        pass
+
+new = [['구', '한국인', '외국인', '외국인 비율(%)']]
+
+new.append([i[0], i[1], i[2], foreign])
+print(new)
+
+# 외국인 비율이 3% 초과할 때만 출력해보기
+import re
+
+for i in newPop:
+    foreign = 0
+    try:
+        foreign = round(i[2] / (i[1] + i[2]) * 100, 1)
+        if foreign > 3:
+            print([i[0], i[1], i[2], foreign])
+    except:
+        pass
+
+new = [['구', '한국인', '외국인', '외국인 비율(%)']]
+for i in newPop:
+    foreign = 0
+    try:
+        foreign = round(i[2] / (i[1] + i[2]) * 100, 1)
+        if foreign > 3:
+            new.append(i[0], i[1], i[2], foreign)
+
+    except:
+        pass
+
+usecsv.writecsv('newPop.csv', new)
