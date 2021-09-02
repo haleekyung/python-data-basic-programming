a94ed5cb13a7b38decc1a334a160dcea0b071613
haleekyung <kyungha03@gmail.com>
2021-09-02 23:53:37 +0900

    modify code: switch module

--- Doit-2-basic/usecsv.py
+++ Doit-2-basic/usecsv.py
@@ -1,6 +1,6 @@
 import csv, os
 
-def OpenCsv(filename):
+def opencsv(filename):
     f = open(filename, 'r')
     reader = csv.reader(f)
     output = []
@@ -9,17 +9,18 @@ def OpenCsv(filename):
     f.close()
     return output
 
-def WriteCsv(filename, the_list):
+def writecsv(filename, the_list):
     with open(filename, 'w', newline='') as f:
         a = csv.writer(f, delimiter = ',')
         a.writerows(the_list)
 
-def switch(listNmae):
+def switch(listName):
     for i in listName:
         for j in i:
             try:
-                i[i.index(j)] = float(re.sub(',', ' ', j))
+                i[i.index(j)] = float(re.sub(',', '', j))
             except:
                 pass
     return listName
+
 
