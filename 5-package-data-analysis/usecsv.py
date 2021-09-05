38a7bb954a9893bbb34f7ee95aa67b507918b707
haleekyung <kyungha03@gmail.com>
2021-09-06 01:49:21 +0900

    upload new files

---
+++ 5-package-data-analysis/usecsv.py
@@ -0,0 +1,26 @@
+import csv, os
+
+def opencsv(filename):
+    f = open(filename, 'r')
+    reader = csv.reader(f)
+    output = []
+    for i in reader:
+        output.append(i)
+    f.close()
+    return output
+
+def writecsv(filename, the_list):
+    with open(filename, 'w', newline='') as f:
+        a = csv.writer(f, delimiter = ',')
+        a.writerows(the_list)
+
+def switch(listName):
+    for i in listName:
+        for j in i:
+            try:
+                i[i.index(j)] = float(re.sub(',', '', j))
+            except:
+                pass
+    return listName
+
+
--- Data/test.py
+++
@@ -1,6 +0,0 @@
-import os
-print(__file__)
-print(os.path.realpath(__file__))
-print(os.path.abspath(__file__))
-
-os.getcwd()
